0aefe73886b21ef22c4088beed472c743b4c7b94
Sri venkatesha mani <srivenkateshmani@gmail.com>
2025-08-29 18:23:25 -0600

    Add counting sort algorithm

---
+++ Counting_sort.py
@@ -0,0 +1,24 @@
+# Time Complexity: O(n + k)
+# Space Complexity: O(k)
+
+# k - max of the array
+# Note - This is just for positive arrays
+
+arr = [-5, 3, 2, 1, -3, -3, 7, 2, 2]
+
+def counting_sort(arr):
+    n = len(arr)
+    maxx = max(arr)
+    counts = [0] * (maxx + 1)
+
+    for x in arr:
+        counts[x] += 1
+    
+    i = 0
+    for c in range(maxx + 1):
+        while counts[c] > 0:
+            arr[i] = c
+            i += 1
+            counts[c] -= 1
+
+print(counting_sort(arr))
